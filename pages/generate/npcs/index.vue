7634486ca7e0e067f14a8d9eea4bf67a3f94db5d
Leandro Bezerra <leandro.b.03@gmail.com>
2025-02-26 19:14:49 -0300

    New layout

--- pages/generate/npcs/index.vue
+++ pages/generate/npcs/index.vue
@@ -1,24 +1,65 @@
 <script setup lang="ts">
-import { panel } from '@primeuix/themes/aura/accordion';
-
 const config = useRuntimeConfig();
 
 const npc = reactive({
-  "name": 'Eldrathorn',
-  "age": '22',
-  "race": 'Humano',
-  "gender": 'Masculino',
-  "alignment": 'Neutro Bom',
-  "level": '1',
-  "classes": 'Lutador',
-  "job": 'Taverneiro',
-  "background": 'Nobre',
-  "sexOrientation": 'Heterossexual',
-  "appearance": 'Tatuado',
-  "personality": 'Amigavel',
-  "affiliations": 'Xanathar',
-  "goals": 'Liberdade',
-  "backstory": 'Criação nobre'
+  name: 'Eldrathorn',
+  age: '22',
+  race: 'Humano',
+  gender: 'Masculino',
+  alignment: 'Neutro Bom',
+  level: '1',
+  classes: 'Lutador',
+  job: 'Taverneiro',
+  background: 'Nobre',
+  sexOrientation: 'Heterossexual',
+  appearance: 'Tatuado',
+  personality: 'Amigavel',
+  affiliations: 'Xanathar',
+  goals: 'Liberdade',
+  backstory: 'Criação nobre',
+  armourClass: 15,
+  initiative: 2,
+  speed: 9,
+  hitPoints: 12,
+  hitDice: '1d10',
+  attr: {
+    str: {
+      title: 'attr.str',
+      short: 'STR',
+      value: 16,
+      bonus: 3
+    },
+    dex: {
+      title: 'attr.dex',
+      short: 'DEX',
+      value: 14,
+      bonus: 2
+    },
+    con: {
+      title: 'attr.con',
+      short: 'CON',
+      value: 15,
+      bonus: 2
+    },
+    int: {
+      title: 'attr.int',
+      short: 'INT',
+      value: 10,
+      bonus: 0
+    },
+    wis: {
+      title: 'attr.wis',
+      short: 'WIS',
+      value: 9,
+      bonus: -1
+    },
+    cha: {
+      title: 'attr.cha',
+      short: 'CHA',
+      value: 13,
+      bonus: 1
+    }
+  }
 });
 const { data: age, status: statusAge, error: errorAge, refresh: refreshAge, clear: clearAge } = await useAsyncData(
   'age',
@@ -166,6 +207,19 @@ const onFormSubmit = ({ valid }) => {
   }
 };
 </script>
+
+<style>
+.striped-bg {
+  background-color: #ccd1d6;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 1.25rem 10px;
+  background-image: linear-gradient(135deg, #e0e4e8 25%, #d9dde1 25%, #d9dde1 50%, #e0e4e8 50%, #e0e4e8 75%, #d9dde1 75%, #d9dde1 100%);
+  background-size: 10px 10px;
+}
+</style>
 
 <template>
   <Panel header="Generate NPC" class="w-full shadow-sm" toggleable :collapsed="panelColapsed">
@@ -404,69 +458,121 @@ const onFormSubmit = ({ valid }) => {
     <div class="bg-surface-0 dark:bg-surface-900 p-6 shadow rounded">
       <div class="text-3xl font-medium text-surface-900 dark:text-surface-0 mb-2">{{ $t('generate.npcs.title') }}</div>
       <div class="font-medium text-surface-500 dark:text-surface-300 mb-4">{{ $t('generate.npcs.description') }}</div>
-      <section class="info mb-2 w-full lg:w-[340px] h-20 px-4 py-2 flex flex-row justify-between">
-        <div class="w-auto">
-          <div class="text-xs font-medium text-surface-500 dark:text-surface-300">{{ $t('common.name') }}</div>
-          <div class="text-base font-semibold text-surface-900 dark:text-surface-0">{{ npc.name }}</div>
-          <div class="text-xs font-light text-surface-900 dark:text-surface-0">{{ `${npc.age} ${$t('common.years')}` }}</div>
-        </div>
-        <div class="">
-          <div class="text-sm font-light text-surface-900 dark:text-surface-0">{{ npc.race }}</div>
-          <div class="text-sm font-medium text-surface-900 dark:text-surface-0">{{ npc.classes }}</div>
-          <div class="text-sm font-medium text-surface-900 dark:text-surface-0">{{ npc.job ? npc.job : '-' }}</div>
-        </div>
-      </section>
-      <div class="flex flex-row justify-start mb-2 gap-2">
-        <section class="w-auto flex flex-row gap-2 mb-1">
-          <div v-for="index in 6" class="ability_card h-[95px] w-[81px]">
-            <div class="px-3 py-1">
-              <div class="flex items-center justify-center my-1">
-                <span v-tooltip.top="'STR'" class="uppercase text-[10px] font-semibold text-gray-500">Strength</span>
+      <div class="flex flex-col lg:flex-row">
+        <div class="w-full lg:w-1/2 border-r border-gray-400 dark:border-gray-300">
+          <section class="info mb-2 w-full lg:w-[340px] h-20 px-4 py-2 flex flex-row justify-between">
+            <div class="w-auto">
+              <div class="text-xs font-medium text-surface-500 dark:text-surface-300">{{ $t('common.name') }}</div>
+              <div class="text-base font-semibold text-surface-900 dark:text-surface-0">{{ npc.name }}</div>
+              <div class="text-xs font-light text-surface-900 dark:text-surface-0">{{ `${npc.age} ${$t('common.years')}` }}</div>
+            </div>
+            <div class="">
+              <div class="text-sm font-light text-surface-900 dark:text-surface-0">{{ npc.race }}</div>
+              <div class="text-sm font-medium text-surface-900 dark:text-surface-0">{{ npc.classes }}</div>
+              <div class="text-sm font-medium text-surface-900 dark:text-surface-0">{{ npc.job ? npc.job : '-' }}</div>
+            </div>
+          </section>
+          <div class="flex flex-row flex-wrap justify-start mb-2 gap-2">
+            <section class="w-auto flex flex-row gap-2 mb-1">
+              <div v-for="attr in npc.attr" class="ability_card h-[95px] w-[81px]">
+                <div class="px-3 py-1">
+                  <div class="flex items-center justify-center my-1">
+                    <span v-tooltip.top="attr.short" class="uppercase text-[9px] font-semibold text-gray-500">{{ $t(attr.title) }}</span>
+                  </div>
+                  <div class="flex items-center justify-center w-full rounded border border-gray-400 dark:border-gray-300 mb-1 h-8">
+                      <span class="text-xl">{{ attr.bonus > 0 ? `+${attr.bonus}` : attr.bonus }}</span>
+                  </div>
+                  <div class="flex items-center justify-center pt-1">{{ attr.value }}</div>
+                </div>
               </div>
-              <div class="flex items-center justify-center w-full rounded border border-gray-400 dark:border-gray-300 mb-1 h-8">
-                <span class="text-xl">+3</span>
+            </section>
+            <section class="h-[95px] w-[81px] armor_class_card p-2 flex flex-col justify-center items-center uppercase">
+              <span class="text-[8px] relative top-1 -mt-2 font-semibold text-gray-500">{{ $t('common.armor_i') }}</span>
+              <div class="text-2xl">15</div>
+              <span class="text-[8px] relative -mt-1 font-semibold text-gray-500">{{ $t('common.class_i') }}</span>
+            </section>
+            <section class="h-[89px] w-[94px] proeficiency_card p-2 flex flex-col justify-center items-center uppercase">
+              <span class="text-[10px] font-semibold">{{ $t('common.proeficiency') }}</span>
+              <div class="my-1 text-2xl"><span class="text-gray-500">+</span>2</div>
+              <span class="font-semibold">{{ $t('common.bonus') }}</span>
+            </section>
+            <section class="h-[89px] w-[94px] walking_card p-2 flex flex-col justify-center items-center uppercase">
+              <span class="text-[10px] font-light">{{ $t('common.walking') }}</span>
+              <div class="my-1 text-2xl">9 <span class="text-gray-500 lowercase">m.</span></div>
+              <span class="text-[10px] font-semibold">{{ $t('common.speed') }}</span>
+            </section>
+            <section class="h-[89px] w-[300px] hp_card py-2 px-4">
+              <div class="flex flex-row justify-between items-center">
+                <span class="text-[10px] font-light">{{ $t('common.hit_points') }}</span>
+                <span class="text-[10px] font-light">{{ $t('common.hit_dice') }}</span>
               </div>
-              <div class="flex items-center justify-center pt-1">16</div>
+              <div class="flex flex-row justify-between items-center">
+                <span class="text-2xl font-semibold">12</span>
+                <span class="text-2xl font-semibold">1d10</span>
+              </div>
+            </section>
+          </div>
+          <div class="flex flex-row justify-start gap-4">
+            <section class="basic_info_card w-[475px] h-[500px] flex flex-col justify-start items-stretch gap-2 py-2 px-4">
+              <div class="mb-6">
+                <h2 class="text-2xl mb-2">{{ $t('common.description') }}</h2>
+                <p class="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur lorem condimentum, vestibulum leo non, fermentum enim. Nam at accumsan justo, quis consequat justo.</p>
+                <p class="font-light">Nullam cursus velit at eleifend sodales. Ut vehicula malesuada neque, sed gravida libero euismod id.</p>
+                <p class="font-light">Sed mattis lacus sit amet ligula vehicula consequat</p>
+                <p class="font-light">Nunc sit amet consectetur nisi</p>
+                <p class="font-light">Integer ultricies nunc ante, eget interdum sem maximus ut</p>
+              </div>
+              <div>
+                <h2 class="text-2xl mb-2">{{ $t('common.appearance') }}</h2>
+                <p class="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur lorem condimentum, vestibulum leo non, fermentum enim. Nam at accumsan justo, quis consequat justo.</p>
+                <p class="font-light">Nullam cursus velit at eleifend sodales. Ut vehicula malesuada neque, sed gravida libero euismod id.</p>
+              </div>
+            </section>
+            <section class="basic_info_card w-[475px] h-[500px] flex flex-col justify-start items-stretch gap-2 py-2 px-4">
+              <div class="mb-6">
+                <h2 class="text-2xl mb-2">{{ $t('common.description') }}</h2>
+                <p class="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur lorem condimentum, vestibulum leo non, fermentum enim. Nam at accumsan justo, quis consequat justo.</p>
+                <p class="font-light">Nullam cursus velit at eleifend sodales. Ut vehicula malesuada neque, sed gravida libero euismod id.</p>
+                <p class="font-light">Sed mattis lacus sit amet ligula vehicula consequat</p>
+                <p class="font-light">Nunc sit amet consectetur nisi</p>
+                <p class="font-light">Integer ultricies nunc ante, eget interdum sem maximus ut</p>
+              </div>
+              <div>
+                <h2 class="text-2xl mb-2">{{ $t('common.appearance') }}</h2>
+                <p class="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur lorem condimentum, vestibulum leo non, fermentum enim. Nam at accumsan justo, quis consequat justo.</p>
+                <p class="font-light">Nullam cursus velit at eleifend sodales. Ut vehicula malesuada neque, sed gravida libero euismod id.</p>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div class="inset-shadow-sm w-full lg:w-1/2 h-auto rounded-r inset-shadow-gray-500 striped-bg">
+          <div class="w-full lg:w-[450px] h-auto lg:h-[750px] bg-white shadow rounded overflow-hidden">
+            <div class="header bg-gray-800 p-4 flex flex-row justify-between items-center">
+              <h2 class="text-3xl text-gray-50">{{ npc.name }}</h2>
+              <div class="flex flex-col">
+                <small class="text-gray-50">{{ npc.race }}</small>
+                <small class="text-gray-50">{{ npc.classes }}</small>
+                <small class="text-gray-50">{{ npc.job }}</small>
+              </div>
+            </div>
+            <div class="body p-4">
+              <table class="table-auto w-full border border-gray-300 rounded">
+                <tr class="border-b border-gray-300">
+                  <td class="bg-gray-200 p-1 rounded-tl w-[35%] text-right">{{ `${$t('common.armor_i')} ${$t('common.class_i')}` }}</td>
+                  <td class="bg-gray-100 p-1 rounded-tr">{{ npc.armourClass }}</td>
+                </tr>
+                <tr class="border-b border-gray-300">
+                  <td class="bg-gray-200 p-1 text-right">{{ $t('common.hit_points') }}</td>
+                  <td class="bg-gray-100 p-1">{{ npc.hitPoints }} <small class="text-xs">{{ npc.hitDice }}</small></td>
+                </tr>
+                <tr class="">
+                  <td class="bg-gray-200 p-1 rounded-bl text-right">{{ $t('common.speed') }}</td>
+                  <td class="bg-gray-100 p-1 rounded-br">{{ npc.speed }} m</td>
+                </tr>
+              </table>
             </div>
           </div>
-        </section>
-        <section class="h-[89px] w-[94px] proeficiency_card p-2 flex flex-col justify-center items-center uppercase">
-          <span class="text-[10px] font-semibold">{{ $t('common.proeficiency') }}</span>
-          <div class="my-1 text-2xl"><span class="text-gray-500">+</span>2</div>
-          <span class="font-semibold">{{ $t('common.bonus') }}</span>
-        </section>
-        <section class="h-[89px] w-[94px] walking_card p-2 flex flex-col justify-center items-center uppercase">
-          <span class="text-[10px] font-light">{{ $t('common.walking') }}</span>
-          <div class="my-1 text-2xl">30 <span class="text-gray-500 lowercase">m.</span></div>
-          <span class="text-[10px] font-semibold">{{ $t('common.speed') }}</span>
-        </section>
-        <section class="h-[89px] w-[300px] hp_card py-2 px-4">
-          <div class="flex flex-row justify-between items-center">
-            <span class="text-[10px] font-light">{{ $t('common.hit_points') }}</span>
-            <span class="text-[10px] font-light">{{ $t('common.hit_dice') }}</span>
-          </div>
-          <div class="flex flex-row justify-between items-center">
-            <span class="text-2xl font-semibold">12</span>
-            <span class="text-2xl font-semibold">1d10</span>
-          </div>
-        </section>
-      </div>
-      <div class="flex flex-row justify-start">
-        <section class="basic_info_card w-[475px] h-[500px] flex flex-col justify-start items-stretch gap-2 py-2 px-4">
-          <div class="mb-6">
-            <h2 class="text-2xl mb-2">{{ $t('common.description') }}</h2>
-            <p class="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur lorem condimentum, vestibulum leo non, fermentum enim. Nam at accumsan justo, quis consequat justo.</p>
-            <p class="font-light">Nullam cursus velit at eleifend sodales. Ut vehicula malesuada neque, sed gravida libero euismod id.</p>
-            <p class="font-light">Sed mattis lacus sit amet ligula vehicula consequat</p>
-            <p class="font-light">Nunc sit amet consectetur nisi</p>
-            <p class="font-light">Integer ultricies nunc ante, eget interdum sem maximus ut</p>
-          </div>
-          <div>
-            <h2 class="text-2xl mb-2">{{ $t('common.appearance') }}</h2>
-            <p class="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur lorem condimentum, vestibulum leo non, fermentum enim. Nam at accumsan justo, quis consequat justo.</p>
-            <p class="font-light">Nullam cursus velit at eleifend sodales. Ut vehicula malesuada neque, sed gravida libero euismod id.</p>
-          </div>
-        </section>
+        </div>
       </div>
     </div>
   </div>
