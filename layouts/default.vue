bd4e7f9f9d39685c5fc8cf33829d27115c6a76ae
Leandro Bezerra <leandro@optimumtech.ia>
2025-04-01 15:26:04 -0300

    Vercel speed Insights

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
+
 <template>
   <Analytics/>
   <SpeedInsights/>
